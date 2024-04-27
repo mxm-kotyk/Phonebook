cfb9cfceb5cb0dfc7c89010eb8f6967c238fe916
Max Kotyk <mxm.kotyk@gmail.com>
2024-04-27 17:42:06 +0200

    Changed BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HelmetProvider>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="goit-react-hw-08-phonebook">
+          <BrowserRouter basename="Phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
