cddf892b2a38cce7fcb93747eff94c7ab9232f31
Max Kotyk <mxm.kotyk@gmail.com>
2024-10-15 17:08:44 +0200

    Change backend address

--- src/redux/authApi.js
+++ src/redux/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://connections-api.herokuapp.com',
+    baseUrl: 'https://connections-api.goit.global',
   }),
   tagTypes: ['Auth'],
   endpoints: builder => ({
--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://connections-api.herokuapp.com',
+    baseUrl: 'https://connections-api.goit.global',
   }),
   tagTypes: ['Contact'],
   endpoints: builder => ({
